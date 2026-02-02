71ebb4d858f7809874622d53eecbde624cd8c1e3
nettnott <nett.work.stud@gmail.com>
2026-02-02 13:24:43 +0300

    dobavlena bd no ona ne rabotat

---
+++ Lists.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISIPGorlanovWPF
+{
+    internal static class Lists
+    {
+        public static List<Film> filmsList = Core.Context.Film.ToList();
+        public static List<Session> sessionsList = Core.Context.Session.ToList();
+
+    }
+}
